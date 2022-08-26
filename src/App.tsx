c675435e6e73d0b1d987b4b5542c42bd201e7c26
theborakompanioni <theborakompanioni+github@gmail.com>
2022-08-27 01:01:42 +0200

    feat: my position marker is draggable

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import React, { PropsWithChildren, useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import { MapContainer, Marker, Popup, Rectangle, ScaleControl, TileLayer, useMap, ZoomControl } from 'react-leaflet'
 import { MinimapControl } from './Minimap'
 import 'leaflet/dist/leaflet.css'
 import './App.css'
-import { ControlPosition, LatLngBoundsExpression } from 'leaflet'
+import { ControlPosition, LatLngBoundsExpression, LatLngExpression, Marker as LeafletMarker } from 'leaflet'
 import { fetchBlockHashByHeight, fetchBlockTipHeight, geohash, LatLng, throwError } from './utils'
 
 const TILE_URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
@@ -23,6 +23,30 @@ const BLOCKHEIGHT_TO_HASH_MAP: { [key: number]: string } = {
 function Minimap({ position }: { position: ControlPosition }) {
   const map = useMap()
   return <MinimapControl parentMap={map} position={position} zoom={1} />
+}
+
+interface DraggableMarkerProps {
+  position: LatLngExpression
+  onDragEnd: (marker: LeafletMarker) => void
+}
+function DraggableMarker({ position, onDragEnd, children }: PropsWithChildren<DraggableMarkerProps>) {
+  const markerRef = useRef<LeafletMarker>(null)
+  const eventHandlers = useMemo(
+    () => ({
+      dragend: () => {
+        if (markerRef.current) {
+          onDragEnd(markerRef.current)
+        }
+      },
+    }),
+    []
+  )
+
+  return (
+    <Marker draggable={true} eventHandlers={eventHandlers} position={position} ref={markerRef}>
+      {children}
+    </Marker>
+  )
 }
 
 function PanToMapCenter({ center }: { center?: LatLng }) {
@@ -45,22 +69,6 @@ function App() {
   const [browserCurrentPositionError, setBrowserCurrentPositionError] = useState<GeolocationPositionError | undefined>(
     undefined
   )
-  const [myPosition, setMyPosition] = useState<LatLng | undefined>(undefined)
-
-  const rectangleBounds = useMemo(() => {
-    if (!myPosition) return
-
-    return [
-      [Math.floor(myPosition[0]), Math.floor(myPosition[1])],
-      [Math.floor(myPosition[0]) + 1, Math.floor(myPosition[1]) + 1],
-    ] as LatLngBoundsExpression
-  }, [myPosition])
-
-  const rectangle = useMemo(() => {
-    if (!rectangleBounds) return
-
-    return <Rectangle bounds={rectangleBounds} pathOptions={{ color: 'black' }} />
-  }, [rectangleBounds])
 
   useEffect(() => {
     if ('geolocation' in navigator) {
@@ -84,8 +92,27 @@ function App() {
     }
   }, [])
 
+  const [myPosition, setMyPosition] = useState<LatLng | undefined>(undefined)
+
+  const myPositionMarker = useMemo(() => {
+    if (!myPosition) return
+
+    return (
+      <DraggableMarker
+        position={myPosition}
+        onDragEnd={(marker) => {
+          const latLng = marker.getLatLng()
+          setMyPosition([latLng.lat, latLng.lng])
+        }}
+      >
+        <Popup>
+          You ({myPosition[0]}, {myPosition[1]})
+        </Popup>
+      </DraggableMarker>
+    )
+  }, [myPosition])
+
   useEffect(() => {
-    console.log('aaaa')
     if (browserCurrentPosition) {
       const latLng: LatLng = [browserCurrentPosition.coords.latitude, browserCurrentPosition.coords.longitude]
       setMyPosition(latLng)
@@ -94,17 +121,27 @@ function App() {
     }
   }, [browserCurrentPosition, browserCurrentPositionError])
 
-  const myPositionMarker = useMemo(() => {
+  useEffect(() => {
     if (!myPosition) return
 
-    return (
-      <Marker position={myPosition}>
-        <Popup>
-          You ({myPosition[0]}, {myPosition[1]})
-        </Popup>
-      </Marker>
-    )
+    const msg = ['Your position changed to:', `${myPosition}`].join('\n')
+    console.info(msg)
   }, [myPosition])
+
+  const rectangleBounds = useMemo(() => {
+    if (!myPosition) return
+
+    return [
+      [Math.floor(myPosition[0]), Math.floor(myPosition[1])],
+      [Math.floor(myPosition[0]) + 1, Math.floor(myPosition[1]) + 1],
+    ] as LatLngBoundsExpression
+  }, [myPosition])
+
+  const rectangle = useMemo(() => {
+    if (!rectangleBounds) return
+
+    return <Rectangle bounds={rectangleBounds} pathOptions={{ color: 'black' }} />
+  }, [rectangleBounds])
 
   const [loading, setLoading] = useState(true)
   const [blockTipHeight, setBlockTipHeight] = useState<number | null>(null)
@@ -122,7 +159,9 @@ function App() {
 
     return (
       <Marker position={geohashPosition}>
-        <Popup autoClose={false}>{blockHash}</Popup>
+        <Popup>
+          {blockHash} ({geohashPosition[0]}, {geohashPosition[1]})
+        </Popup>
       </Marker>
     )
   }, [blockHash, geohashPosition])
@@ -241,9 +280,10 @@ function App() {
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url={TILE_URL}
             />
-            {myPositionMarker}
-            {geohashPositionMarker}
             {rectangle}
+            {geohashPositionMarker}
+            {myPositionMarker}
+
             <PanToMapCenter center={geohashPosition} />
           </MapContainer>
         </div>
