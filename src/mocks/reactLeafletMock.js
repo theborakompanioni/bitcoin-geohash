76d6684305e441fdf55be7026c28714e275f3695
theborakompanioni <theborakompanioni+github@gmail.com>
2022-10-01 22:19:57 +0200

    test: mock react-leaflet module

---
+++ src/mocks/reactLeafletMock.js
@@ -0,0 +1,12 @@
+module.exports = {
+    Marker: () => (<></>),
+    Popup: () => (<></>),
+    Rectangle: () => (<></>),
+    MapContainer: () => (<></>),
+    ScaleControl: () => (<></>),
+    TileLayer: () => (<></>),
+    ZoomControl: () => (<></>),
+    Rectangle: () => (<></>),
+    useMap: () => {}, 
+    useMapEvents: () => {}, 
+}
